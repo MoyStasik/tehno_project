75ae0ca9aa907bd3af0d0d9cb7cc9469ab2ff6e9
Stas <stas_kriger@mail.ru>
2024-04-08 20:46:52 +0300

    fix

--- app/views.py
+++ app/views.py
@@ -6,8 +6,8 @@ from django.shortcuts import render, redirect
 QUESTIONS = [
     {
         "id": i,
-        "title": f"Question {i+1}",
-        "text": f"This is question number {i+1}"
+        "title": f"Question {i}",
+        "text": f"This is question number {i}"
     } for i in range(20)
 ]
 ANSWERS = [
@@ -16,25 +16,30 @@ ANSWERS = [
         "text": f"This is Answer number {i+1}"
     } for i in range(10)
 ]
+def paginate(object_list, request, per_page = 10):
+    page_num = request.GET.get('page', 1)
+    paginator = Paginator(object_list, per_page )
+    page_obj = paginator.page(page_num)
+    return page_obj, Paginator(object_list, per_page)
+
 
 def index(request):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 5)
-    page_obj = paginator.page(page_num)
+    page_count = 5
+    page_obj, paginator = paginate(QUESTIONS, request, page_count)
     return render(request, "index.html", {"questions": page_obj, "pagin": paginator})
 
 def hot(request):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS[5:], 5)
-    page_obj = paginator.page(page_num)
+    page_count = 5
+    start = 5
+    page_obj, paginator = paginate(QUESTIONS[start:], request, page_count)
     return render(request, "hot.html", {"questions": page_obj, "pagin": paginator})
 
 
 def question(request, question_id):
      item = QUESTIONS[question_id]
-     page_num = request.GET.get('page', 1)
-     paginator = Paginator(ANSWERS, 2)
-     page_obj = paginator.page(page_num)
+     page_count = 2
+
+     page_obj, paginator = paginate(ANSWERS, request, page_count)
      return render(request, "question.html", {"question": item,"questions": page_obj, "answers": page_obj, "pagin": paginator})
 
 def login(request):
@@ -53,7 +58,7 @@ def settings(request):
     return render(request, "settings.html")
 
 def tag(request, tag_name):
-    page_num = request.GET.get('page', 1)
-    paginator = Paginator(QUESTIONS, 3)
-    page_obj = paginator.page(page_num)
+    page_count = 3
+
+    page_obj, paginator = paginate(QUESTIONS, request, page_count)
     return render(request, "tag.html", {"questions": page_obj,"pagin": paginator, "tag_name": tag_name})
